00b6b242c936b4248bb60ddcab156e3883ba731e
Piyushkumardas <piyush.dasinfo@gmail.com>
2023-06-20 22:51:14 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [dicenumber, setdicenumber] = useState(2)
+  const [dicenumber2, setdicenumber2] = useState(1)
+
+  const refreshDice = () => {
+    const randomnumber = Math.floor(Math.random() * 6) + 1;
+    setdicenumber(randomnumber)
+    const randomnumber2 = Math.floor(Math.random() * 6) + 1;
+    setdicenumber2(randomnumber2)
+
+  
+}  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Dice Roller Application</h1>
+      <center><img width={300} height={300} src={require(`./assets/${dicenumber}.png`)} ></img>
+      <img width={300} height={300} src={require(`./assets/${dicenumber2}.png`)} ></img>
+      <br/>
+      <button onClick={()=> refreshDice()} className='button'>Roll</button>
+      </center>
+      
+    </>
+    
+    
+  )
 }
 
 export default App;
